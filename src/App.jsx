1b968e55e423ebc619a52548b67fc55f32fd97a4
Yuta Mizuno <mizunoyuta1014@gmail.com>
2024-01-14 15:46:03 +0900

    make sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Books";
+import Home from "./components/Home";
 import Want from "./components/Want";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
@@ -10,12 +10,14 @@ function App() {
   return (
     <Router>
       <Header />
+      <div className="mainarea">
       <Sidebar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/books" element={<Books />}></Route>
         <Route path="/want" element={<Want />}></Route>
       </Routes>
+      </div>
     </Router>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <nav>
+      <Link to="/">ホーム</Link>
       <Link to="/books">蔵書</Link>
       <Link to="/want">読みたい本</Link>
     </nav>
