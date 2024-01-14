26dbad6b4dad51178da4830c9e0494256a8e08d8
Yuta Mizuno <mizunoyuta1014@gmail.com>
2024-01-14 15:46:03 +0900

    make sidebar.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+    <div>
+      <div>
+        <p className="homeguidance">サイドバーから表示したい項目を選択</p>
+      </div>
+      <div>
+        <p>年度を選択できるように設計予定</p>
+        <p>人増やすならログイン機能も付ける</p>
+      </div>
+    </div>
+
+    </>
+
+  );
 };
 
 export default Home;
