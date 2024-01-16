91e8fc7a8a0dc0955688a49ca7185e91215dc461
Yuta Home <mizunoyuta1014@gmail.com>
2024-01-16 21:35:08 +0900

    追加メニューを修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="titlebelt">
-      <p className="title">年間読書計画</p>
+      <p className="title">Bookshelf</p>
     </div>
   );
 };
--- src/components/Want.jsx
+++ src/components/Want.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Want = () => {
-  return <div>Want</div>;
+  return (
+    <>
+      <div>
+        <p>読みたい本を入力していく</p>
+        <p>楽天apiで検索機能を追加</p>
+      </div>
+    </>
+  );
 };
 
 export default Want;
