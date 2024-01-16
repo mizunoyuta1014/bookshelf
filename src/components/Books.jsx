8e0e37ed2d9527e125471d4e6d60a43b3da4d80d
Yuta Home <mizunoyuta1014@gmail.com>
2024-01-16 21:35:08 +0900

    追加メニューを修正

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,6 +5,12 @@ import "./Book.css";
 const Books = () => {
   const [isPopupOpen, setPopupOpen] = useState(false);
 
+  const [bookTitle, setBookTitle] = useState();
+  const [author, setAuthor] = useState();
+  const [bookPlace, setbookPlace] = useState();
+  const [category, setCategory] = useState();
+  const [priority, setPriority] = useState();
+
   const handleAddClick = () => {
     setPopupOpen(true);
   };
@@ -13,17 +19,27 @@ const Books = () => {
     setPopupOpen(false);
   };
 
+  const selectBookPlace = (e) => {
+    setbookPlace(e.target.value);
+  };
+
+  const selectCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
   return (
     <div className="bookpage">
       <div className="add-book">
-        <button onClick={handleAddClick}>追加</button>
+        <button onClick={handleAddClick} className="add-button">
+          追加
+        </button>
       </div>
       {isPopupOpen && (
         <>
           <div className="overlay" onClick={handleCloseClick} />
           <div className="add-menu">
             <div className="add-menu-upper">
-              <p>追加</p>
+              <p className="add-title">追加</p>
               <button className="close-button" onClick={handleCloseClick}>
                 <IoClose />
               </button>
@@ -38,12 +54,25 @@ const Books = () => {
                 <input type="text" />
               </li>
               <li>
-                <div>形式</div>
-                <input type="text" />
+                <div>保管場所</div>
+                <select name="genre" id="genre" onChange={selectBookPlace}>
+                  <option value="home">自宅</option>
+                  <option value="electron">電子書籍</option>
+                  <option value="other">その他</option>
+                </select>
               </li>
               <li>
                 <div>カテゴリ</div>
-                <input type="text" />
+                <select
+                  name="genre"
+                  onChange={selectCategory}
+                  id="getcategorybox"
+                >
+                  <option value="programing">プログラミング</option>
+                  <option value="busines">ビジネス</option>
+                  <option value="Cons">コンサル</option>
+                  <option value="other">その他</option>
+                </select>
               </li>
               <li>
                 <div>優先度</div>
