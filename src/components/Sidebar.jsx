349e8d54a3cf09a78974eaca1aa766da6f4bbcf5
Yuta Home <mizunoyuta1014@gmail.com>
2024-01-21 23:27:49 +0900

    dbへの送信処理完了。受信処理途中

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <nav>
       <Link to="/">ホーム</Link>
-      <Link to="/books">蔵書</Link>
+      <Link to="/books">2024年読書記録</Link>
       <Link to="/want">読みたい本</Link>
     </nav>
   );
